4b67d1a36b00bdb3f44280c538b757ab4b6657eb
杉原 匡燿 <kyoyo.sugihara@ai.info.gifu-u.ac.jp>
2025-11-26 16:40:54 +0900

    update js

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ import pkg from "agora-access-token";
 const { RtcTokenBuilder, RtcRole } = pkg;
 
 // QualtricsのURLを許可する
-const allowedOrigin = "https://survey.syd1.qualtrics.com";
-
+const allowedOrigins = new Set([
+  "https://survey.syd1.qualtrics.com",
+  "https://p2ppd-qualtrics-static.onrender.com"
+]);
 const corsOptions = {
   origin: allowedOrigin,
   methods: ["GET", "POST", "OPTIONS"],
