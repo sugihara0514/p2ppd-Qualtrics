69793302d52a7178a44a50d1028c2c42bf7c2b56
杉原 匡燿 <kyoyo.sugihara@ai.info.gifu-u.ac.jp>
2025-12-10 13:43:57 +0900

    update js

--- server/server.js
+++ server/server.js
@@ -333,9 +333,9 @@ app.post("/game/join", async (req, res) => {
 
   // プレイヤーが2人揃ったら録画開始（既に開始済みなら内部でなにもしない）
   // テスト中につき、一時録画停止中
-  // if (g.players.size >= 2) {
-  //   startCloudRecording(channel); // await してもいいが、レスポンスを遅らせたくないなら fire-and-forget でOK
-  // }
+  if (g.players.size >= 2) {
+    startCloudRecording(channel); // await してもいいが、レスポンスを遅らせたくないなら fire-and-forget でOK
+  }
 
   return res.json({
     ok: true,
